1e8206dce712c602f085984d133ec07d9ea42645
leftrightleft <leftrightleft@github.com>
2023-10-04 13:21:22 -0700

    add org name to output files

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     gh = github.Client(gh_token)
     for pr_url in gh.get_diff_urls(repo):
         pr_number = pr_url.split('/')[-1]  # Extract PR number from the URL
-        repo_name = repo.split('/')[-1]  # Extract repo name from the repo string
-        filename = f"data/{repo_name}-{pr_number}"
+        org_name, repo_name = repo.split('/')  # Extract org name and repo name from the repo string
+        filename = f"data/{org_name}-{repo_name}-{pr_number}"
         try:
             diff_content = gh.get_diff(pr_url)
             with open(filename, 'w') as f:
