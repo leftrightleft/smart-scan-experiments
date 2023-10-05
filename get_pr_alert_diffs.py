8e2c845bf18b3c206c65fe1c80cfd2964ef55731
leftrightleft <leftrightleft@github.com>
2023-10-05 10:54:52 -0700

    rename to get_diffs

--- get_pr_alert_diffs.py
+++ get_pr_alert_diffs.py
@@ -1,1 +1,21 @@
-import gh
+import gh as github
+import sys
+
+gh_token = sys.argv[1]
+repo = sys.argv[2]
+
+def main():
+    gh = github.Client(gh_token)
+    instance_urls = gh.get_alert_instance_urls(repo)
+    # print(len(instance_urls))
+    pr_nums = []
+    for url in instance_urls:
+        pr_alerts = gh.get_pr_alerts(url)
+        for pr in pr_alerts:
+            # alert_num = pr.split('/')[-2]  # Extract alert number from the PR URL
+            if pr not in pr_nums:
+                pr_nums.append(pr)
+    print(pr_nums)
+
+if __name__ == "__main__":
+    main()
--- gh.py
+++ gh.py
@@ -40,3 +40,32 @@ class Client:
             logging.error(e)
             raise Exception(e)
         
+    def get_alert_instance_urls(self, repo, tool_name="CodeQL"):
+        alert_urls = []
+        states=["open", "closed", "dismissed", "fixed"]
+        for state in states:
+            url = f"{self.endpoint}/repos/{repo}/code-scanning/alerts?state={state}&tool_name={tool_name}&per_page=100"
+            while url:
+                response = requests.get(url, headers=self.headers)
+                alerts = json.loads(response.text)
+                for alert in alerts:
+                    alert_urls.append(alert['instances_url'])
+                if 'next' in response.links:
+                    url = response.links['next']['url']
+                else:
+                    url = None
+        return alert_urls
+    
+    def get_pr_alerts(self, instance_url):
+        prs = []
+        while instance_url:
+            response = requests.get(instance_url, headers=self.headers)
+            instances = json.loads(response.text)
+            for instance in instances:
+                if 'refs/pull' in instance['ref']:
+                    prs.append(instance['ref'].split('/')[2])
+            if 'next' in response.links:
+                instance_url = response.links['next']['url']
+            else:
+                instance_url = None
+        return prs
